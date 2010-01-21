0de77a52b796ff3bc5bb7e59bf426b6eb2db191a
Adam McCrea <adam@adamlogic.com>
2010-01-21 09:53:37 -0500

    offset() on an svg element is broken in Safari

--- javascripts/app.js
+++ javascripts/app.js
@@ -10,9 +10,10 @@ $(function() {
     rect.attr({fill: '#f00', 'stroke-width': 0});
 
     $('body').mousemove(function(e) {
-      var width = e.pageX - rect.jq().offset().left;
-      var height = e.pageY - rect.jq().offset().top;
-      rect.attr({width: width, height: height});
+      var width = e.pageX - rect.offset().left;
+      var height = e.pageY - rect.offset().top;
+      rect.attr({ width:  Math.max(0, width), 
+                  height: Math.max(0, height) });
     });
   }).mouseup(function(e) {
     $('body').unbind('mousemove');
@@ -27,11 +28,17 @@ Raphael.el.jq = function() {
   return el;
 };
 
+Raphael.el.offset = function() {
+  var paperOffset = $('#canvas').offset();
+  return { left: paperOffset.left + this.attr('x'),
+           top: paperOffset.top + this.attr('y') }
+};
+
 Raphael.el.draggable = function() {
   var dragging, offsetX, offsetY;
 
   this.jq().mousedown(function(e) {
-    dragging = $(this);
+    dragging = $(this).raph();
     offsetX = e.pageX - dragging.offset().left;
     offsetY = e.pageY - dragging.offset().top;
     $('body').mousemove(drag);
@@ -45,10 +52,10 @@ Raphael.el.draggable = function() {
   function drag(e) {
     var x = e.pageX - offsetX - $('#canvas').offset().left;
     var y = e.pageY - offsetY - $('#canvas').offset().top;
-    dragging.raphael().attr({x: x, y: y});
+    dragging.attr({x: x, y: y});
   }
 };
 
 $.fn.extend({
-  raphael: function() { return this.data('raphael') }
+  raph: function() { return this.data('raphael') }
 });
