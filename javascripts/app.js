12c608ce9837df34ede63a61bef3e6983c500685
Adam McCrea <adam@adamlogic.com>
2010-01-23 14:40:30 -0500

    Clip and image with an ellipse

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,22 +1,31 @@
 $(function() {
 
-  var canvas = $('#canvas'), rect;
+  var canvas = $('#canvas'), x, y, rect, clipWrapper, clip;
   paper = Raphael(canvas[0], canvas.width(), canvas.height());
 
   canvas.mousedown(function(e) {
-    var x = e.pageX - canvas.offset().left;
-    var y = e.pageY - canvas.offset().top;
-    rect = paper.rect(x, y, 1, 1);
+    x = e.pageX - canvas.offset().left;
+    y = e.pageY - canvas.offset().top;
+    rect = paper.image('http://www.google.com/intl/en_ALL/images/logo.gif', x, y, 1, 1);
     rect.attr({fill: '#f00', 'stroke-width': 0});
 
+    rect.attr({'clip-rect': '0 0 0 0'});
+    clipWrapper = $(rect.jq().attr('clip-path').match(/\((.*)\)/)[1]);
+    clipWrapper.find('rect').remove();
+    clip = paper.ellipse(x, y, 0, 0);
+    clipWrapper.append(clip);
+
     $('body').mousemove(function(e) {
-      var width = e.pageX - rect.offset().left;
-      var height = e.pageY - rect.offset().top;
-      rect.attr({ width:  Math.max(0, width), 
-                  height: Math.max(0, height) });
+      var width = e.pageX - rect.offset().left,
+          height = e.pageY - rect.offset().top,
+          width = Math.max(0, width),
+          height = Math.max(0, height);
+      rect.attr({ width: width, height: height });
+      clip.attr({ cx: x + width/2, cy: y + height/2, rx: width/2, ry: height/2 });
     });
   }).mouseup(function(e) {
     $('body').unbind('mousemove');
+
     rect.draggable();
   });
 
